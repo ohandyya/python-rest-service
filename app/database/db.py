d2f8b24550f8f99fb8c4c582ce2e49bf7e755056
atseng <atseng@pindropsecurity.com>
2021-05-02 19:11:19 -0400

    Add log

--- app/database/db.py
+++ app/database/db.py
@@ -1,12 +1,19 @@
 import os
+import logging
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 
 DEFAULT_DATABASE_URL = "sqlite:////app/sql_app.db"
 SQLALCHEMY_DATABASE_URL = os.environ.get("DB_URL", DEFAULT_DATABASE_URL)
 
 
+logger.info(f"DB_URL: {SQLALCHEMY_DATABASE_URL}")
 if SQLALCHEMY_DATABASE_URL.startswith("sqlite:"):
     engine = create_engine(
         SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
